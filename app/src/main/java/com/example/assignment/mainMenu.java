e77694877417b7d397a3aecbcab7525822b5b57a
FMLneverlucky <66565733+FMLneverlucky@users.noreply.github.com>
2024-11-21 16:59:29 +0800

    added placeholder mainmenu page built and tested using phone to be working

---
+++ app/src/main/java/com/example/assignment/mainMenu.java
@@ -0,0 +1,13 @@
+package com.example.assignment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class mainMenu extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainmenu);
+    }
+}

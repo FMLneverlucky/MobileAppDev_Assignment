6793bf59bb2392c01e835844ba50746ca764f180
FMLneverlucky <jingjie0506@gmail.com>
2024-12-06 17:12:03 +0800

    add temporary bg and changed to portrait mode

--- app/src/main/java/com/example/assignment/edits/AppGameScene.java
+++ app/src/main/java/com/example/assignment/edits/AppGameScene.java
@@ -1,15 +1,26 @@
 package com.example.assignment.edits;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 
 import androidx.annotation.ColorInt;
 
+import com.example.assignment.R;
+import com.example.assignment.mgp2d.core.GameActivity;
 import com.example.assignment.mgp2d.core.GameScene;
 
 public class AppGameScene extends GameScene {
-
-
+    private Bitmap _backgroundBitmap;
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        int screenWidth = GameActivity.instance.getResources().getDisplayMetrics().widthPixels;
+        int screenHeight = GameActivity.instance.getResources().getDisplayMetrics().heightPixels;
+        Bitmap BackgroundBitmap = BitmapFactory.decodeResource(GameActivity.instance.getResources(), R.drawable.kyuu_pic);
+        _backgroundBitmap = Bitmap.createScaledBitmap(BackgroundBitmap, screenWidth, screenHeight, true);
+    }
     @Override
     public void onUpdate(float dt) {
 
@@ -17,6 +28,6 @@ public class AppGameScene extends GameScene {
 
     @Override
     public void onRender(Canvas canvas) {
-        canvas.drawColor(Color.parseColor("#fcd977"));
+        canvas.drawBitmap(_backgroundBitmap,0,0,null);
     }
 }

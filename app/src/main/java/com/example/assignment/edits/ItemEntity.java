ef0bfeb42ff50e4b87b6f943ca453e4ba734dcb1
FMLneverlucky <jingjie0506@gmail.com>
2024-12-12 13:45:17 +0800

    work in progress attempting to make item move only one direction until reset

--- app/src/main/java/com/example/assignment/edits/ItemEntity.java
+++ app/src/main/java/com/example/assignment/edits/ItemEntity.java
@@ -18,7 +18,7 @@ public class ItemEntity extends GameEntity{
     private final Rect _srcRect;    //source rectangle -> covers the relevant area in sprite image (in context of animated sprite)
     private final Rect _dstRect;    //destination rectangle -> rectangle that reside in screen
     private GameScene.flickDirection pointerFlickDirection = GameScene.flickDirection.NONE;  //i cant exactly call a game scene protected variable here, so jus switch case the enum passed in as a diff data type -> changed function to public since its a pain in the ass, also going to be repetitive to keep declaring different methods in every class (context: was a string type before change)
-
+    protected boolean get_direction = true; //when start, direction is none, so allow receiving of direction from game scene class
     private static final int speed = 200; //speed of position update
 
     private final Vector2 startPosition = new Vector2((float)GameActivity.instance.getResources().getDisplayMetrics().widthPixels / 2, (float)GameActivity.instance.getResources().getDisplayMetrics().heightPixels /2); //will always start and reset at same place -> i would static this if i could but its only avaliable for int variables smh, also this fucking piece of shit is somehow updating somewhere im not aware of
@@ -38,43 +38,35 @@ public class ItemEntity extends GameEntity{
 
     @Override
     public void onUpdate(float dt) {
+        //current bug: receive flick direction is set in main game scene every frame. this function is overwriting set to none direction when reset position in here.
+        //possible fixes:
         //reset position of item if it reaches any screen boundary
-        if (screenBoundaryCollision())
-            _position = new Vector2(540, 1088.5f); //i tried to set position back to start position but that fucking variable is receiving position update from god knows where. i seriously have no idea where so im doing the illegal
+        if (screenBoundaryCollision()) {
+            _position = new Vector2((float) GameActivity.instance.getResources().getDisplayMetrics().widthPixels / 2, (float) GameActivity.instance.getResources().getDisplayMetrics().heightPixels / 2); //makiing it so it resets to center on different device
+            //want to make it so that item can only move in one direction until reset -> reset item direction here
+            pointerFlickDirection = GameScene.flickDirection.NONE;
+            get_direction = true; //allow setting of direction again
+        }
 
         //update position
-        //pain since want to move in axis but its vector 2 -> to implement normalized direction to check which axis to move
-        //phone display starts at top left corner; increase value in right and down direction
-        //init midpoint of screen to check which ddirection to move -> actually can use start direction since item spawn at middle
-        //TODO: CHANGE POINTERFLICKDIRECTION TO ENUM (also consider using switch case here)
-        /*
-        if (pointerFlickDirection.x > (float)GameActivity.instance.getResources().getDisplayMetrics().widthPixels / 2)
-            _position.x -= speed * dt;
-        else if (pointerFlickDirection.x < (float)GameActivity.instance.getResources().getDisplayMetrics().widthPixels / 2)
-            _position.x += speed * dt;
-
-        if (pointerFlickDirection.y > (float)GameActivity.instance.getResources().getDisplayMetrics().heightPixels / 2)
-            _position.y -= speed * dt;
-        else if (pointerFlickDirection.y < (float)GameActivity.instance.getResources().getDisplayMetrics().heightPixels / 2)
-            _position.y += speed * dt;
-         */
-
-        switch (pointerFlickDirection)
+        //item should only move in one direction until reset -> only move when direction is not "none"
+        if (pointerFlickDirection != GameScene.flickDirection.NONE)
         {
-            case LEFT: //none
-                _position.x -= speed * dt;
-                return;
-            case RIGHT:
-                _position.x += speed * dt;
-                return;
-            case UP:
-                _position.y -= speed * dt;
-                return;
-            case DOWN:
-                _position.y += speed * dt;
-                return;
-            default:
-
+            switch (pointerFlickDirection) {
+                case LEFT: //none
+                    _position.x -= speed * dt;
+                    return;
+                case RIGHT:
+                    _position.x += speed * dt;
+                    return;
+                case UP:
+                    _position.y -= speed * dt;
+                    return;
+                case DOWN:
+                    _position.y += speed * dt;
+                    return;
+                default:
+            }
         }
         //something about position and vector having different point of origin -> one is top left one is bottom right?? anyways problem here if move in inverse direction
     }
@@ -96,8 +88,11 @@ public class ItemEntity extends GameEntity{
     }
     public void receiveFlickDirection(GameScene.flickDirection direction){ //to be changed: vector 2 to enum
         pointerFlickDirection = direction;
-        Log.d("RECEIVED", "flick direction received");  //debugging dont mind me
+        //Log.d("RECEIVED", "flick direction received");  //debugging dont mind me
+        get_direction = false;  //bool solely to stop flick direction variable above from getting updates O|<; "i already got the direction to go, so dont change my route"
     }
+
+    public boolean get_direction_permit() {return get_direction;} //idk anymore
 
     protected boolean screenBoundaryCollision()
     {
